56b67f44c165d41be4458f94c3dda5612436d6c9
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-11-05 17:01:32 -0300

    Updating bot activity on ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,11 @@
 module.exports = async (client) => {
   client.logger.ready(`Bot's ready. Running with ${client.users.size - 1} users in ${client.guilds.size} servers.`);
+
+  client.user.setPresence({
+    game: {
+      name: 'what u say',
+      type: 'LISTENING'
+    },
+    status: 'dnd'
+  });
 };
